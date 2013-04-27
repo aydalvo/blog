ee2dfdf247c9b02ace416f8c49787ab4e8ca33fc
Aydalvo <aydalvo.nery@gmail.com>
2013-04-27 16:37:04 -0300

    remove
    remove

--- Blog/WebContent/WEB-INF/resources/insert_assunto.sql
+++
@@ -1,9 +0,0 @@
-use blog;
-
-insert into assunto (descricao, request_mapping) values ("Agile", "/Blog/comente-sobre/categoria");
-insert into assunto (descricao, request_mapping) values ("Java", "/Blog/comente-sobre/java");
-insert into assunto (descricao, request_mapping) values ("Engenharia de Software", "/Blog/comente-sobre/engenharia-software");
-insert into assunto (descricao, request_mapping) values ("RUP", "/Blog/comente-sobre/rup");
-insert into assunto (descricao, request_mapping) values ("TDD", "/Blog/comente-sobre/tdd");
-
-commit;
